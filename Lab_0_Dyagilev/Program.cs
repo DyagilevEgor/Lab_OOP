b9e532142f42c1190e845551c61425f9bef5e63f
Dyagilev Egor <ead29@tpu.ru>
2024-12-05 14:37:43 +0300

    feature(Square): добавлен метод для расчета площади квадрата
    создан функционал для вычисления площади квадрата на основе введенной длины стороны
    
    - добавлен метод CalculateSquareArea

--- Lab_0_Dyagilev/Program.cs
+++ Lab_0_Dyagilev/Program.cs
@@ -11,5 +11,22 @@ namespace Lab_0_Dyagilev
         static void Main(string[] args)
         {
         }
+
+        static void CalculateSquareArea()
+        {
+            Console.Write("Введите длину стороны квадрата: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a > 0)
+            {
+                double area = a * a;
+                Console.WriteLine($"Площадь квадрата: {area}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено недопустимое значение для стороны квадрата.");
+            }
+            Console.ReadLine();
+        }
     }
 }
