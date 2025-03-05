3fadde25f0b97591d37a4dcea19a5898676a69dd
Dyagilev Egor <ead29@tpu.ru>
2025-03-05 18:24:47 +0300

    feature(PLb1): добавлен класс AddConsolePerson

---
+++ Lb1/Lb1/AddConsolePerson.cs
@@ -0,0 +1,76 @@
+﻿using PersonLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace Lb1
+{
+    /// <summary>
+    /// Класс, предназначенный для
+    /// добавленя персоны через консоль
+    /// </summary>
+    public static class AddConsolePerson
+    {
+        /// <summary>
+        /// Добавление Person через консоль        
+        /// </summary>
+        /// <returns>Новая персона</returns>
+        public static Person NewPerson()
+        {
+            Person newPerson = new Person("Im", "Batman", 123, Gender.Male);
+            List<Action> actions = new List<Action>()
+            {
+                new Action(() =>
+                {
+                    Console.Write("Name: ");
+                    newPerson.Name = Console.ReadLine();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Surname: ");
+                    newPerson.Surname = Console.ReadLine();
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Age: ");
+                    string ageString = Console.ReadLine();
+                    if(!Int32.TryParse(ageString, out int age))
+                    {
+                        throw new ArgumentException("The age must be an " +
+                            "integer value!");
+                    }
+                    newPerson.Age = age;
+                }),
+                new Action(() =>
+                {
+                    Console.Write("Gender (1 - Male, 2 - Female): ");
+                    int gender = Int32.Parse(Console.ReadLine());
+                    Person.CheckingSex(gender);
+                    newPerson.Gender = (Gender)Enum.Parse(
+                        typeof(Gender), Convert.ToString(gender));
+                }),
+            };
+            actions.ForEach(SetValue);
+            return newPerson;
+        }
+
+        // <summary>
+        /// Получение пользовательский ввода
+        /// и задание параметра
+        /// </summary>
+        public static void SetValue(Action action)
+        {
+            while (true)
+            {
+                try
+                {
+                    action.Invoke();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"\n{e.Message}\n");
+                }
+            }
+        }
+    }
+}
--- Lb1/Lb1/Program.cs
+++ Lb1/Lb1/Program.cs
@@ -1,15 +1,132 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PersonLibrary;
 
 namespace Lb1
 {
-    class Program
+    /// <summary>
+    /// Основной класс  
+    /// </summary>
+    internal class Program
     {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        /// <param name="args">Параметры</param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Press any button to start...");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.WriteLine("1. Two lists of persons are creating, " +
+                "each contains three persons...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            var firstList = new PersonList();
+            var secondList = new PersonList();
+
+            var firstArray = new Person[]
+            {
+                new Person("Dexter", "Morgan", 40, Gender.Male),
+                new Person("Donald John", "Trump", 78, Gender.Male),
+                new Person("Tony", "Stark", 45, Gender.Male),
+            };
+
+            var secondArray = new Person[]
+            {
+                new Person("Sarah-Jessica", "Parker", 59, Gender.Female),
+                new Person("Kevin", "Smith", 54, Gender.Male),
+                new Person("Alex", "Lion", 15, Gender.Male),
+            };
+
+            firstList.AddArrayOfPeople(firstArray);
+            secondList.AddArrayOfPeople(secondArray);
+
+            Console.WriteLine("Two lists of persons have been sucсessfully created!");
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine("2. Displaying the contents of " +
+                "each list to the console...");
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+
+            Console.WriteLine("3. Adding a new person to the first list... ");
+            firstList.AddPerson(new Person("Homer", "Simpson", 40, Gender.Male));
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+            Console.WriteLine("A new person has been successfully added" +
+                " to the first list!");
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine("4. Copying the second person from the first" +
+                " list to the end of the second list... ");
+            secondList.AddPerson(firstList.FindByIndex(1));
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+            Console.WriteLine("Now the same person is on both lists!");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("5. Removing the second person from the first list...");
+            firstList.RemovePersonByIndex(1);
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+            Console.WriteLine("Removing a person from the 1st list did not " +
+                "delete the same person in the 2nd list!");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("6. Clearing the second list...");
+            secondList.ClearList();
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine("All persons have been successfully removed from the 2nd list!");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("7. Let's add a new person " +
+                "to the second list from keyboard...");
+            Console.WriteLine();
+            secondList.AddPerson(AddConsolePerson.NewPerson());
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+
+            Console.WriteLine("8. Add a random person " +
+                "to the second list...");
+            Person randPerson = RandomPerson.GetRandomPerson();
+            secondList.AddPerson(randPerson);
+            ShowListOfPersons(firstList, secondList);
+            Console.WriteLine();
+            Console.WriteLine("Press any button to exit...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Вывод списка персон
+        /// </summary>
+        /// <param name="firstList">Список один</param>
+        /// <param name="secondList">Список два</param>
+        public static void ShowListOfPersons(PersonList firstList, PersonList secondList)
+        {
+            var personLists = new PersonList[]
+            {
+                firstList,
+                secondList
+            };
+            Console.ReadKey();
+            for (int i = 0; i < personLists.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"List {i + 1}");
+                Console.WriteLine();
+
+                for (int j = 0; j < personLists[i].Count; j++)
+                {
+                    Console.WriteLine(personLists[i].FindByIndex(j).Info);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
