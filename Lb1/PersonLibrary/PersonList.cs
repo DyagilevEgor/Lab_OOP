744ab5a5bfc515821b3ff474e1fe5f24d42b5ff4
Dyagilev Egor <ead29@tpu.ru>
2025-03-05 18:12:05 +0300

    feature(PersonLibrary): добавлен класс PersonList

---
+++ Lb1/PersonLibrary/PersonList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonLibrary
+{
+    /// <summary>
+    /// Класс, представляющий список людей (Person).
+    /// Предоставляет методы для управления списком: добавление, удаление,
+    /// поиск и очистка.
+    /// </summary>
+    public class PersonList
+    {
+        /// <summary>
+        /// Приватное поле для хранения списка людей
+        /// </summary>
+        private List<Person> _people;
+
+        /// <summary>
+        /// Конструктор для инициализации нового списка людей.
+        /// </summary>
+        public PersonList()
+        {
+            _people = new List<Person>();
+        }
+
+        /// <summary>
+        ///  Метод для добавления человека в список.
+        /// </summary>
+        /// <param name="person">Объект типа Person, который нужно добавить
+        /// в список.</param>
+        public void AddPerson(Person person)
+        {
+            _people.Add(person);
+        }
+
+        /// <summary>
+        /// Добавление нескольких людей
+        /// </summary>
+        /// <param name="persons">Массив людей</param>
+        public void AddArrayOfPeople(Person[] persons)
+        {
+            foreach (Person person in persons)
+            {
+                AddPerson(person);
+            }
+        }
+
+
+        /// <summary>
+        /// Метод для удаления человека из списка по объекту.
+        /// </summary>
+        /// <param name="person">Объект типа Person, который нужно удалить из
+        /// списка.</param>
+        public void RemovePerson(Person person)
+        {
+            _people.Remove(person);
+        }
+
+        /// <summary>
+        /// Метод для удаления человека из списка по индексу.
+        /// </summary>
+        /// <param name="index">Индекс элемента, который нужно удалить.</param>
+        public void RemovePersonByIndex(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                _people.RemoveAt(index);
+            }
+        }
+
+        /// <summary>
+        /// Метод для возврата индекса указанного человека в списке.
+        /// </summary>
+        /// <param name="person">Объект типа Person, индекс которого нужно
+        /// найти.</param>
+        /// <returns>Индекс объекта в списке, если он найден; в противном
+        /// случае — -1.</returns>
+        public int GetIndexOfPerson(Person person)
+        {
+            return _people.IndexOf(person);
+        }
+
+        /// <summary>
+        /// Поиск элемента по индексу
+        /// </summary>
+        /// <param name="index">Индекс человека</param>
+        /// <returns>возвращает значение по указанному индексу</returns> 
+        public Person FindByIndex(int index)
+        {
+            if (index >= 0 && index < _people.Count)
+            {
+                return _people[index];
+            }
+            else
+            {
+                throw new Exception("The index you requested " +
+                    "does not exist!");
+            }
+        }
+
+        /// <summary>
+        /// Метод для очистки всего списка.
+        /// </summary>
+        public void ClearList()
+        {
+            _people.Clear();
+        }
+
+        /// <summary>
+        /// Метод для получения количества элементов в списке.
+        /// </summary>
+        public int Count => _people.Count;
+    }
+}
