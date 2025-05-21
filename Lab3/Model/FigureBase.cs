f4fe8f6704ff460a74373e3bb39ac473653987ea
Dyagilev Egor <ead29@tpu.ru>
2025-05-21 21:06:34 +0300

    feature(Model): добавлен класс FigureBase

---
+++ Lab3/Model/FigureBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Базовый класс 
+    /// для всех фигур
+    /// </summary>
+    public abstract class FigureBase
+    {
+        /// <summary>
+        /// Расчёт площади
+        /// </summary>
+        public abstract double Area { get; }
+
+        /// <summary>
+        /// Проверка числа
+        /// </summary>
+        /// <param name="number">Число для проверки</param>
+        /// <returns>Корректное число</returns>
+        public static double CheckingNumber(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Величина должна " +
+                    "быть положительным числом!");
+            }
+            else
+            {
+                return number;
+            }
+        }
+    }
+}
