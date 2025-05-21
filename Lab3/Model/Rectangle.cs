aff6d7288b732225ce71b8f861dc1c86a3700b39
Dyagilev Egor <ead29@tpu.ru>
2025-05-21 21:19:41 +0300

    feature(Model): добавлен класс Rectangle

---
+++ Lab3/Model/Rectangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Класс прямоугольник 
+    /// </summary>
+    public class Rectangle : FigureBase
+    {
+        /// <summary>
+        /// Длина
+        /// </summary>
+        private double _length;
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private double _width;
+
+        /// <summary>
+        /// Длина
+        /// </summary>
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                CheckingNumber(value);
+                _length = value;
+            }
+        }
+
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                CheckingNumber(value);
+                _width = value;
+            }
+        }
+
+        /// <summary>
+        /// Вычисление площади прямоугольника
+        /// </summary>
+        /// <retutns>Площадь прямоугольника</retutns>
+        public override double Area
+        {
+            get
+            {
+                return Math.Round(Length * Width, 3);
+            }
+        }
+    }
+}
